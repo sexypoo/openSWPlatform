f8ee876fd7143658baa819a1722f47b90d228d60
sexypoo <goeun_goeun_goeun_@naver.com>
2025-11-08 19:56:27 +0900

    feat : frontend css 깨짐 현상 수정 + backend 기능과 연결

--- app/pages.py
+++ app/pages.py
@@ -14,9 +14,13 @@ def login():
 def signup():
     return render_template("signup.html")
 
-@pages_bp.route("/list")
+@pages_bp.route("/product")
 def view_list():
-    return render_template("list.html")
+    return render_template("products.html")
+
+@pages_bp.route("/product/1") # be 적용 전 임시 기능
+def view_product():
+    return render_template("product_detail.html")
 
 @pages_bp.route("/review")
 def view_review():
@@ -29,3 +33,11 @@ def reg_item():
 @pages_bp.route("/reg_reviews")
 def reg_review():
     return render_template("reg_reviews.html")
+
+@pages_bp.route("/mypage")
+def view_mypage():
+    return render_template("mypage.html");
+
+@pages_bp.route("/mypage/wishlist")
+def view_wishlist():
+    return render_template("wishlist.html");
